851e1099e96da7a68e530ea205b0a2743a0b619d
tadicbb <tadicbb@gmail.com>
2021-11-23 17:11:15 +0100

    I started doing tests. but I felt very bad and did not finish, I did my best

---
+++ Tasks.Tests/ServicesTests/TasksServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tasks.BL;
+using Tasks.BL.Interfaces;
+using Tasks.Data.Data;
+
+namespace Tasks.Tests.Tasts
+{
+    [TestClass]
+    class TasksServiceTests
+    {
+        readonly Mock<ITaskService> _taskService;
+        public TasksServiceTests(Mock<ITaskService>  taskService)
+        {
+            _taskService = taskService;
+            
+        }
+        Task tasks = new Task
+        {
+            Id = 0,
+            PriorityId = 0,
+            Desription = "desc",
+            Name = "name",
+            ProjectId = 0,
+            StatusId = 0,
+
+        };
+
+        [TestMethod]
+        public void GetTask_Successful()
+        {
+            //asert
+            
+            _taskService.Setup()
+            //act 
+
+
+
+        }
+    }
+}
